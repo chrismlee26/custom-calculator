282625e098a88aed959ce8991696e1dc7f1ad423
CHRIS_LEE <chrismlee87@gmail.com>
2020-09-03 14:43:03 -0700

    commit 3 cleaner code

--- omelettes.py
+++ omelettes.py
@@ -4,42 +4,33 @@ print("It doesn't matter, " + name)
 
 omelette_type = input("French? or American Omelette? ")
 
-#if ({omelette_type} = "american"):
-#omelette_type = input("Nobody likes American Omelettes try again ")
-## french omelette proceeds !while loop!
-    ## "Ok, how many eggs?"
-#if french then howmany_eggs, if American, go back up one level.
-## american omelette says "wrong choice" and goes back to asking
-
-def pan_egg_ratio(howmany_eggs):
+def pan_egg_ratio(num_eggs):
   pan_size = 0
-  if int(howmany_eggs) == 2:
+  if int(num_eggs) == 2:
     pan_size = 6
-      return pan_size
-  elif int(howmany_eggs) == 3:
+  elif int(num_eggs) == 3:
     pan_size = 8
-      return pan_size
-  elif int(howmany_eggs) == 4:
+  elif int(num_eggs) == 4:
     pan_size = 12
-      return pan_size
+  return pan_size
 
-#pan_size = int(howmany_eggs) * 3    <---- backup code for failed function above
+#pan_size = int(num_eggs) * 3    <---- backup code for failed function above
 
 while omelette_type != "french":
  omelette_type = input("Nobody likes American omelettes try \"french\" ")
 
-howmany_eggs = input("Ok, 2 or 3 or 4 eggs? ")
-print(f"Ok calculating for {howmany_eggs} eggs")
+num_eggs = input("Ok, 2 or 3 or 4 eggs? ")
+print(f"Ok calculating for {num_eggs} eggs")
 
-pan_size = pan_egg_ratio(howmany_eggs)
+pan_size = pan_egg_ratio(num_eggs)
 
-howmuch_butter = int(howmany_eggs)*.5
+num_butter = int(num_eggs)*.5
 
-print(f"Ok so {omelette_type} omelette and {howmany_eggs} eggs. Here's your receipe")
+print(f"Ok so {omelette_type} omelette and {num_eggs} eggs. Here's your receipe")
 
 print("1. Whisk eggs completely.")
 
-print(f"2. Grab a {pan_size}\" pan, eggs and {howmuch_butter} tablespoons of butter. On medium heat, place butter until melted and slightly frothy. Pour in the {howmany_eggs} whisked eggs and turn the heat to low.")
+print(f"2. Grab a {pan_size}\" pan, eggs and {num_butter} tablespoons of butter. On medium heat, place butter until melted and slightly frothy. Pour in the {num_eggs} whisked eggs and turn the heat to low.")
 
 print("3. Stir into a circular pattern until eggs begin to form. Continually lift sides of eggs and shake the pan for allow uncooked eggs and butter to form under the egg that has already set.")
 
