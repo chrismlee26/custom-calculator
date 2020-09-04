2adce9b24341d5974adf21047ba9236c052d318e
CHRIS_LEE <chrismlee87@gmail.com>
2020-09-03 18:02:49 -0700

    recipe changes for clarity

--- omelettes.py
+++ omelettes.py
@@ -14,7 +14,7 @@ def pan_egg_ratio(num_eggs):
     pan_size = 12
   return pan_size
 
-#pan_size = int(num_eggs) * 3    <---- backup code for failed function above 
+#pan_size = int(num_eggs) * 3    <---- backup code incase failed function above, no longer needed but I like it
 
 while omelette_type != "french":
  omelette_type = input("Nobody likes American omelettes try \"french\" ")
@@ -32,7 +32,7 @@ print("1. Whisk eggs completely.")
 
 print(f"2. Grab a {pan_size}\" pan, eggs and {num_butter} tablespoons of butter. On medium heat, place butter until melted and slightly frothy. Pour in the {num_eggs} whisked eggs and turn the heat to low.")
 
-print("3. Stir into a circular pattern until eggs begin to form. Continually lift sides of eggs and shake the pan for allow uncooked eggs and butter to form under the egg that has already set.")
+print("3. Stir into a circular pattern while eggs begin to form. Continually lift sides of eggs and shake the pan for allow uncooked eggs and butter to form under the egg that has already set. You will need more movement than you think, but not so much that nothing happens :)")
 
 print(f"4. Once 60-70% formed, add salt and fold the {omelette_type} omelette into thirds.")
 
